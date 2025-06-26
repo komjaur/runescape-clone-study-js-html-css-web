63b18c978d635047caa6da4fbe82f9521a2b371a
ojassokfdjoksad <komjaur@gmail.com>
2025-06-26 11:23:56 +0200

    Improve README with instructions and upcoming features

---
+++ RunescapeWebGame/js/units.js
@@ -0,0 +1,16 @@
+const unitDatabase = {
+  goblin: {
+    name: 'Goblin',
+    hp: 10,
+    attack: 2,
+    defence: 0,
+    drops: ['Bronze Dagger', 'Bones']
+  },
+  skeleton: {
+    name: 'Skeleton',
+    hp: 15,
+    attack: 3,
+    defence: 1,
+    drops: ['Bones', 'Iron Sword']
+  }
+};
